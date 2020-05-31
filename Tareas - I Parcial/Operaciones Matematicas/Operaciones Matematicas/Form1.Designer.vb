36ed22e29acd7541c7a39a15e7acb220942dd271
Johan Urbina <Johan Urbina@LAPTOP-GK4VNBDO>
2020-05-31 16:52:04 -0600

    SE adjunto el ejercicio tarea de Operaciones Matematicas

---
+++ Tareas - I Parcial/Operaciones Matematicas/Operaciones Matematicas/Form1.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.txtSuma = New System.Windows.Forms.TextBox()
+        Me.txtmultiplicacion = New System.Windows.Forms.TextBox()
+        Me.txtResta = New System.Windows.Forms.TextBox()
+        Me.txtdivision = New System.Windows.Forms.TextBox()
+        Me.txtValor2 = New System.Windows.Forms.TextBox()
+        Me.txtValor1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnCalcular.Location = New System.Drawing.Point(259, 83)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 0
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = False
+        '
+        'btnSalir
+        '
+        Me.btnSalir.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.btnSalir.Location = New System.Drawing.Point(259, 137)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 1
+        Me.btnSalir.Text = "S&alir"
+        Me.btnSalir.UseVisualStyleBackColor = False
+        '
+        'txtSuma
+        '
+        Me.txtSuma.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.txtSuma.Enabled = False
+        Me.txtSuma.Location = New System.Drawing.Point(132, 173)
+        Me.txtSuma.Name = "txtSuma"
+        Me.txtSuma.Size = New System.Drawing.Size(100, 20)
+        Me.txtSuma.TabIndex = 2
+        Me.txtSuma.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtmultiplicacion
+        '
+        Me.txtmultiplicacion.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.txtmultiplicacion.Enabled = False
+        Me.txtmultiplicacion.Location = New System.Drawing.Point(132, 244)
+        Me.txtmultiplicacion.Name = "txtmultiplicacion"
+        Me.txtmultiplicacion.Size = New System.Drawing.Size(100, 20)
+        Me.txtmultiplicacion.TabIndex = 3
+        Me.txtmultiplicacion.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtResta
+        '
+        Me.txtResta.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.txtResta.Enabled = False
+        Me.txtResta.Location = New System.Drawing.Point(132, 209)
+        Me.txtResta.Name = "txtResta"
+        Me.txtResta.Size = New System.Drawing.Size(100, 20)
+        Me.txtResta.TabIndex = 4
+        Me.txtResta.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtdivision
+        '
+        Me.txtdivision.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.txtdivision.Enabled = False
+        Me.txtdivision.Location = New System.Drawing.Point(132, 287)
+        Me.txtdivision.Name = "txtdivision"
+        Me.txtdivision.Size = New System.Drawing.Size(100, 20)
+        Me.txtdivision.TabIndex = 5
+        Me.txtdivision.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtValor2
+        '
+        Me.txtValor2.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.txtValor2.Location = New System.Drawing.Point(90, 104)
+        Me.txtValor2.Name = "txtValor2"
+        Me.txtValor2.Size = New System.Drawing.Size(100, 20)
+        Me.txtValor2.TabIndex = 6
+        '
+        'txtValor1
+        '
+        Me.txtValor1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.txtValor1.Location = New System.Drawing.Point(90, 45)
+        Me.txtValor1.Name = "txtValor1"
+        Me.txtValor1.Size = New System.Drawing.Size(100, 20)
+        Me.txtValor1.TabIndex = 7
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label1.Location = New System.Drawing.Point(22, 48)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Valor 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label2.Location = New System.Drawing.Point(22, 104)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Valor 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label3.Location = New System.Drawing.Point(72, 173)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(34, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Suma"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label4.Location = New System.Drawing.Point(72, 212)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(35, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Resta"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label5.Location = New System.Drawing.Point(55, 251)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(71, 13)
+        Me.Label5.TabIndex = 12
+        Me.Label5.Text = "Multiplicacion"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label6.Location = New System.Drawing.Point(72, 287)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(44, 13)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Division"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DodgerBlue
+        Me.ClientSize = New System.Drawing.Size(375, 334)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtValor1)
+        Me.Controls.Add(Me.txtValor2)
+        Me.Controls.Add(Me.txtdivision)
+        Me.Controls.Add(Me.txtResta)
+        Me.Controls.Add(Me.txtmultiplicacion)
+        Me.Controls.Add(Me.txtSuma)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Name = "Form1"
+        Me.Text = "Resta"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents txtSuma As TextBox
+    Friend WithEvents txtmultiplicacion As TextBox
+    Friend WithEvents txtResta As TextBox
+    Friend WithEvents txtdivision As TextBox
+    Friend WithEvents txtValor2 As TextBox
+    Friend WithEvents txtValor1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ Tareas - I Parcial/Operaciones Matematicas/Operaciones Matematicas/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim valor1 As Integer
+        Dim valor2 As Integer
+        Dim suma As Integer
+        Dim resta As Integer
+        Dim multiplicacion As Integer
+        Dim division As Double
+
+        valor1 = Val(txtValor1.Text)
+        valor2 = Val(txtValor2.Text)
+
+        suma = valor1 + valor2
+        multiplicacion = valor1 * valor2
+        division = valor1 / valor2
+        resta = valor1 - valor2
+
+        txtSuma.Text = suma
+        txtResta.Text = resta
+        txtmultiplicacion.Text = multiplicacion
+        txtdivision.Text = division
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        End
+    End Sub
+End Class
