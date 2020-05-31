8085f5afc3672b46d41d37e22c429590b73f5272
Johan Urbina <Johan Urbina@LAPTOP-GK4VNBDO>
2020-05-31 15:33:44 -0600

    Se adjunta el proyecto de primeras validaciones y boton limpiar.

---
+++ Trabajos en Clase  - I Parcial/PrimerasValidaciones/PrimerasValidaciones/Form1.Designer.vb
@@ -0,0 +1,86 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.btnValidar = New System.Windows.Forms.Button()
+        Me.txtNumero = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.Location = New System.Drawing.Point(170, 105)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.btnLimpiar.TabIndex = 0
+        Me.btnLimpiar.Text = "Limpia&r"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'btnValidar
+        '
+        Me.btnValidar.Location = New System.Drawing.Point(56, 105)
+        Me.btnValidar.Name = "btnValidar"
+        Me.btnValidar.Size = New System.Drawing.Size(75, 23)
+        Me.btnValidar.TabIndex = 1
+        Me.btnValidar.Text = "Vali&dar"
+        Me.btnValidar.UseVisualStyleBackColor = True
+        '
+        'txtNumero
+        '
+        Me.txtNumero.Location = New System.Drawing.Point(93, 69)
+        Me.txtNumero.Name = "txtNumero"
+        Me.txtNumero.Size = New System.Drawing.Size(122, 20)
+        Me.txtNumero.TabIndex = 2
+        Me.txtNumero.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(104, 43)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(95, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Ingrese un numero"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(322, 202)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtNumero)
+        Me.Controls.Add(Me.btnValidar)
+        Me.Controls.Add(Me.btnLimpiar)
+        Me.Name = "Form1"
+        Me.Text = "Validacion"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnLimpiar As Button
+    Friend WithEvents btnValidar As Button
+    Friend WithEvents txtNumero As TextBox
+    Friend WithEvents Label1 As Label
+End Class
