ef59da85fd1fef29634337e97985572235b95199
Johan Urbina <Johan Urbina@LAPTOP-GK4VNBDO>
2020-05-31 15:09:09 -0600

    Se adjunto el ejercicio de las uma de 2 valores.

---
+++ Trabajos en Clase  - I Parcial/Ejercicio 1 de Suma/Ejercicio 1 de Suma/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtResultado.TextChanged
+
+    End Sub
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim val1, val2, sum As Double
+
+        val1 = Val(txtValor1.Text)
+        val2 = Val(txtValor2.Text)
+
+        sum = val1 + val2
+
+        txtResultado.Text = sum
+
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        End
+    End Sub
+End Class
