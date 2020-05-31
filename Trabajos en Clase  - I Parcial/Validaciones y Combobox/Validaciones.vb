82c06500b2b1a00dc53a724f9b2f2851e28dcacc
Johan Urbina <Johan Urbina@LAPTOP-GK4VNBDO>
2020-05-31 16:28:00 -0600

    Se adjunto el ejercicio de validaciones con ErrorProvider

---
+++ Trabajos en Clase  - I Parcial/Validaciones y Combobox/Validaciones.vb
@@ -0,0 +1,63 @@
+﻿Imports System.ComponentModel
+
+Public Class Validaciones
+    Private Sub btnValidar_Click(sender As Object, e As EventArgs) Handles btnValidar.Click
+        'Try Catch
+        'ValidateChildren  Botton
+        'Validating     Cajas de texto
+        Try
+            'Si las casillas estan sin  ningun valor
+            'Aplique a todos los elementos>  Producto sea diferente de vacio, precio sea diferente de vacio, Cantidad sea un valor entero
+            If Me.ValidateChildren And txtProducto.Text <> String.Empty And txtPrecio.Text <> String.Empty And Val(txtCantidad.Text) - Int(Val(txtCantidad.Text)) = 0 Then
+                MessageBox.Show("Producto Ingresado", "Formulario Producto", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Else
+                MessageBox.Show("Revise los datos ingresados", "Error en Productos", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub txtProducto_TextChanged(sender As Object, e As EventArgs) Handles txtProducto.TextChanged
+
+    End Sub
+
+    Private Sub txtProducto_Validating(sender As Object, e As CancelEventArgs) Handles txtProducto.Validating
+        If DirectCast(sender, TextBox).TextLength > 0 Then
+            Me.errorValidacion.SetError(sender, "")
+        Else
+            Me.errorValidacion.SetError(sender, "Es un campo obligatorio")
+        End If
+
+    End Sub
+
+    Private Sub Validaciones_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub txtPrecio_TextChanged(sender As Object, e As EventArgs) Handles txtPrecio.TextChanged
+
+    End Sub
+
+    Private Sub txtPrecio_Validating(sender As Object, e As CancelEventArgs) Handles txtPrecio.Validating
+        If DirectCast(sender, TextBox).TextLength > 0 Then
+            Me.errorValidacion.SetError(sender, "")
+        Else
+            Me.errorValidacion.SetError(sender, "Ingrese un precio")
+        End If
+    End Sub
+
+    Private Sub txtProducto_MouseHover(sender As Object, e As EventArgs) Handles txtProducto.MouseHover
+        toolTip.SetToolTip(txtProducto, "Ingrese un producto")
+        toolTip.ToolTipTitle = "Producto"
+        toolTip.ToolTipIcon = ToolTipIcon.Error
+
+    End Sub
+
+    Private Sub txtPrecio_MouseHover(sender As Object, e As EventArgs) Handles txtPrecio.MouseHover
+        toolTip.SetToolTip(txtPrecio, "Ingrese un precio")
+        toolTip.ToolTipTitle = "Precio"
+        toolTip.ToolTipIcon = ToolTipIcon.Error
+
+    End Sub
+End Class
