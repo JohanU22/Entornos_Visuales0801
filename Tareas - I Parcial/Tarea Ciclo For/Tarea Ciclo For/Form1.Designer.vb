74a0f4218ddb700a978c57bc3e5e4857bf44bc53
Johan Urbina <Johan Urbina@LAPTOP-GK4VNBDO>
2020-05-31 17:23:44 -0600

    Se adjunta la tarea correspondiente al Ciclo For

---
+++ Tareas - I Parcial/Tarea Ciclo For/Tarea Ciclo For/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btncalcular = New System.Windows.Forms.Button()
+        Me.txtNumero = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtFactorial = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btncalcular
+        '
+        Me.btncalcular.Location = New System.Drawing.Point(110, 107)
+        Me.btncalcular.Name = "btncalcular"
+        Me.btncalcular.Size = New System.Drawing.Size(114, 36)
+        Me.btncalcular.TabIndex = 0
+        Me.btncalcular.Text = "C&alcular"
+        Me.btncalcular.UseVisualStyleBackColor = True
+        '
+        'txtNumero
+        '
+        Me.txtNumero.Location = New System.Drawing.Point(66, 55)
+        Me.txtNumero.Name = "txtNumero"
+        Me.txtNumero.Size = New System.Drawing.Size(211, 20)
+        Me.txtNumero.TabIndex = 1
+        Me.txtNumero.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(107, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(146, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Ingrese el numero a calcular: "
+        '
+        'txtFactorial
+        '
+        Me.txtFactorial.Location = New System.Drawing.Point(124, 221)
+        Me.txtFactorial.Name = "txtFactorial"
+        Me.txtFactorial.Size = New System.Drawing.Size(100, 20)
+        Me.txtFactorial.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(145, 205)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Resultado"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(361, 297)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtFactorial)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtNumero)
+        Me.Controls.Add(Me.btncalcular)
+        Me.Name = "Form1"
+        Me.Text = "Factorial de un Numero"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btncalcular As Button
+    Friend WithEvents txtNumero As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtFactorial As TextBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Tareas - I Parcial/Tarea Ciclo For/Tarea Ciclo For/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Private Sub btncalcular_Click(sender As Object, e As EventArgs) Handles btncalcular.Click
+        Dim numero, Var, Vi, Vf As Integer
+        Dim fact As Double
+
+        numero = Val(txtNumero.Text)
+        Vi = 1
+        Vf = numero
+        fact = 1
+
+        If numero >= 0 And (IsNumeric(numero)) And (Val(txtNumero.Text) - Int(Val(txtNumero.Text)) = 0) Then
+
+            For Var = Vf To Vi Step -1
+                fact = Var * fact
+            Next Var
+
+            txtFactorial.Text = fact
+            txtFactorial.TextAlign = HorizontalAlignment.Center
+        End If
+
+        If numero < 0 Or (Not IsNumeric(numero)) Or (Val(txtNumero.Text) - Int(Val(txtNumero.Text)) <> 0) Then
+            MsgBox("MATH ERROR ...Debe ingresar un numero entero positivo")
+        End If
+
+        If txtNumero.Text = "" Then
+            MsgBox("Debe ingresar un valor en la casilla")
+
+        End If
+
+    End Sub
+
+    Private Sub txtNumero_TextChanged(sender As Object, e As EventArgs) Handles txtNumero.TextChanged
+
+    End Sub
+
+
+End Class
