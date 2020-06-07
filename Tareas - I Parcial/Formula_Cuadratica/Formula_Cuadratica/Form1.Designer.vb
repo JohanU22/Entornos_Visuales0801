ed53ce6354f39cb672e929562ace20dfb065f16a
Johan Urbina <Johan Urbina@LAPTOP-GK4VNBDO>
2020-06-06 21:02:57 -0600

    Se adjunta el ejercicio de Calculo de Formula Cuadratica

---
+++ Tareas - I Parcial/Formula_Cuadratica/Formula_Cuadratica/Form1.Designer.vb
@@ -0,0 +1,249 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.txtResultado2 = New System.Windows.Forms.TextBox()
+        Me.txtA = New System.Windows.Forms.TextBox()
+        Me.txtB = New System.Windows.Forms.TextBox()
+        Me.txtC = New System.Windows.Forms.TextBox()
+        Me.errorValidacion = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.txtResultado1 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.errorValidacion, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(92, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(229, 158)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(87, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(223, 26)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Formula Cuadratica"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.GroupBox1.Controls.Add(Me.txtC)
+        Me.GroupBox1.Controls.Add(Me.txtB)
+        Me.GroupBox1.Controls.Add(Me.txtA)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.PictureBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(409, 265)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.GroupBox2.Controls.Add(Me.Label7)
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.txtResultado1)
+        Me.GroupBox2.Controls.Add(Me.txtResultado2)
+        Me.GroupBox2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 321)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(409, 115)
+        Me.GroupBox2.TabIndex = 3
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Resultados"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("AR JULIAN", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(65, 201)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(24, 23)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "A"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("AR JULIAN", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(195, 201)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(23, 23)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "B"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("AR JULIAN", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(332, 201)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(22, 23)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "C"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(26, 173)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(135, 16)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Ingrese un numero"
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btnCalcular.Location = New System.Drawing.Point(184, 283)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 4
+        Me.btnCalcular.Text = "C&alcular"
+        Me.btnCalcular.UseVisualStyleBackColor = False
+        '
+        'txtResultado2
+        '
+        Me.txtResultado2.Location = New System.Drawing.Point(254, 52)
+        Me.txtResultado2.Name = "txtResultado2"
+        Me.txtResultado2.Size = New System.Drawing.Size(100, 20)
+        Me.txtResultado2.TabIndex = 0
+        '
+        'txtA
+        '
+        Me.txtA.Location = New System.Drawing.Point(29, 227)
+        Me.txtA.Name = "txtA"
+        Me.txtA.Size = New System.Drawing.Size(100, 20)
+        Me.txtA.TabIndex = 9
+        '
+        'txtB
+        '
+        Me.txtB.Location = New System.Drawing.Point(159, 227)
+        Me.txtB.Name = "txtB"
+        Me.txtB.Size = New System.Drawing.Size(100, 20)
+        Me.txtB.TabIndex = 10
+        '
+        'txtC
+        '
+        Me.txtC.Location = New System.Drawing.Point(293, 227)
+        Me.txtC.Name = "txtC"
+        Me.txtC.Size = New System.Drawing.Size(100, 20)
+        Me.txtC.TabIndex = 11
+        '
+        'errorValidacion
+        '
+        Me.errorValidacion.ContainerControl = Me
+        '
+        'txtResultado1
+        '
+        Me.txtResultado1.Location = New System.Drawing.Point(70, 52)
+        Me.txtResultado1.Name = "txtResultado1"
+        Me.txtResultado1.Size = New System.Drawing.Size(100, 20)
+        Me.txtResultado1.TabIndex = 1
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(89, 31)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(57, 16)
+        Me.Label6.TabIndex = 2
+        Me.Label6.Text = "Valor 1"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(269, 31)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(57, 16)
+        Me.Label7.TabIndex = 3
+        Me.Label7.Text = "Valor 2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ClientSize = New System.Drawing.Size(438, 448)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Calcular Formula Cuadratica"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.errorValidacion, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents txtResultado2 As TextBox
+    Friend WithEvents txtC As TextBox
+    Friend WithEvents txtB As TextBox
+    Friend WithEvents txtA As TextBox
+    Friend WithEvents errorValidacion As ErrorProvider
+    Friend WithEvents txtResultado1 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ Tareas - I Parcial/Formula_Cuadratica/Formula_Cuadratica/Form1.vb
@@ -0,0 +1,64 @@
+﻿Public Class Form1
+    Dim valor_a, valor_b, valor_c, res, res2 As Double
+
+    Private Sub txtA_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtA.Validating
+        If DirectCast(sender, TextBox).TextLength > 0 Then
+            Me.errorValidacion.SetError(sender, "")
+        Else
+            Me.errorValidacion.SetError(sender, "Es un campo obligatorio")
+        End If
+
+    End Sub
+
+    Private Sub txtB_TextChanged(sender As Object, e As EventArgs) Handles txtB.TextChanged
+
+    End Sub
+
+    Private Sub txtB_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtB.Validating
+        If DirectCast(sender, TextBox).TextLength > 0 Then
+            Me.errorValidacion.SetError(sender, "")
+        Else
+            Me.errorValidacion.SetError(sender, "Debe llenar todos los espacios con numeros")
+        End If
+
+    End Sub
+
+    Private Sub txtC_Validating(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles txtC.Validating
+        If DirectCast(sender, TextBox).TextLength > 0 Then
+            Me.errorValidacion.SetError(sender, "")
+        Else
+            Me.errorValidacion.SetError(sender, "Debe ingresar un valor numerico")
+        End If
+
+    End Sub
+
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        valor_a = Val(txtA.Text)
+        valor_b = Val(txtB.Text)
+        valor_c = Val(txtC.Text)
+        Try
+            If Me.ValidateChildren And txtA.Text <> String.Empty And txtB.Text <> String.Empty And txtC.Text <> String.Empty And IsNumeric(txtA.Text) And IsNumeric(txtB.Text) And IsNumeric(txtC.Text) And txtA.Text <> 0 And txtC.Text <> 0 And txtC.Text <> 0 Then
+                res = (-valor_b + (valor_b ^ 2 - (4 * valor_a * valor_c)) ^ 1 / 2) / (2 * valor_a)
+                res2 = (-valor_b - (valor_b ^ 2 - (4 * valor_a * valor_c)) ^ 1 / 2) / (2 * valor_a)
+                txtResultado1.Text = res
+                txtResultado2.Text = res2
+            Else
+                MessageBox.Show("ERROR revise los datos ingresados", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            End If
+        Catch Ex As Exception
+            MsgBox(Ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
